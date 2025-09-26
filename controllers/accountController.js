781e3fed701481a5783a3879b88b18d86a669d5a
jlucas97 <jeanlucascastillo@hotmail.com>
2025-09-26 10:53:06 -0600

    Process registration pre-test

---
+++ controllers/accountController.js
@@ -0,0 +1,72 @@
+const utilities = require("../utilities/")
+const accountModel = require("../models/account-model")
+
+const accountController = {}
+
+/* ****************************************
+ *  Deliver login view
+ * *************************************** */
+accountController.buildLogin = async function (req, res, next) {
+  try {
+    let nav = await utilities.getNav()
+    res.render("account/login", {
+      title: "Login",
+      nav,
+    })
+  } catch (error) {
+    next(error)
+  }
+}
+
+/* ****************************************
+ *  Deliver registration view
+ * *************************************** */
+accountController.buildRegister = async function (req, res, next) {
+  try {
+    let nav = await utilities.getNav()
+    res.render("account/register", {
+      title: "Register",
+      nav,
+      errors: null
+    })
+  } catch (error) {
+    next(error)
+  }
+}
+
+
+/* ****************************************
+*  Process Registration
+* *************************************** */
+accountController.registerAccount = async function (req, res) {
+  let nav = await utilities.getNav()
+  const { account_firstname, account_lastname, account_email, account_password } = req.body
+
+  const regResult = await accountModel.registerAccount(
+    account_firstname,
+    account_lastname,
+    account_email,
+    account_password
+  )
+
+  if (regResult && regResult.rowCount > 0) {
+    req.flash(
+      "notice",
+      `Congratulations, you\'re registered ${account_firstname}. Please log in.`
+    )
+    res.status(201).render("account/login", {
+      title: "Login",
+      nav,
+    })
+  } else {
+    req.flash("notice", "Sorry, the registration failed.")
+    res.status(501).render("account/register", {
+      title: "Register",
+      nav,
+    })
+  }
+}
+
+
+
+module.exports = accountController
